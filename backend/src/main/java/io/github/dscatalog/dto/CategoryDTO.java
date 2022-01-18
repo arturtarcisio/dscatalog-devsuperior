6b6d8fe2f84c7d352b02ddf3b8c513b5592cfaf7
Artur Tarcisio <arturtarcisio1@gmail.com>
2022-01-18 20:52:04 -0300

    DTO

---
+++ backend/src/main/java/io/github/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,23 @@
+package io.github.dscatalog.dto;
+
+import io.github.dscatalog.entities.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO implements Serializable {
+
+    private Long id;
+    private String name;
+
+    public CategoryDTO(Category entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+
+}
--- backend/src/main/java/io/github/dscatalog/services/CategoryService.java
+++ backend/src/main/java/io/github/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package io.github.dscatalog.services;
 
+import io.github.dscatalog.dto.CategoryDTO;
 import io.github.dscatalog.entities.Category;
 import io.github.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -13,7 +16,9 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<Category> findAll () {
-        return repository.findAll();
+    public List<CategoryDTO> findAll () {
+        List<Category> list = repository.findAll();
+        //List<CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+        return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
     }
 }
