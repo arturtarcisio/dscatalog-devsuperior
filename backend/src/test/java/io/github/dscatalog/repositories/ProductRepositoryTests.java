f6d7e0455863bca2ade325dcff5f1f37b37f779d
Artur Tarcisio <arturtarcisio1@gmail.com>
2022-03-02 22:22:49 -0300

    Another case test of delete of ProductRepository

--- backend/src/test/java/io/github/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/io/github/dscatalog/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import io.github.dscatalog.entities.Product;
 
@@ -25,6 +26,16 @@ public class ProductRepositoryTests {
 		Optional<Product> result = repository.findById(existingId);
 		
 		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
+		
+		long nonExistentId = 900L;
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistentId);
+		});
 	}
 
 }
