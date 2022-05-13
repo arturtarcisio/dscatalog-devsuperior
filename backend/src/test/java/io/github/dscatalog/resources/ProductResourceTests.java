2a32bd05f36b2f7d4cd37b2a5aac62047c46e602
Artur Tarcisio <arturtarcisio1@gmail.com>
2022-05-13 06:56:17 -0300

    Comecando testes na camada web

---
+++ backend/src/test/java/io/github/dscatalog/resources/ProductResourceTests.java
@@ -0,0 +1,48 @@
+package io.github.dscatalog.resources;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import io.github.dscatalog.dto.ProductDTO;
+import io.github.dscatalog.services.ProductService;
+import io.github.dscatalog.tests.Factory;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private ProductService service;
+	
+	private PageImpl<ProductDTO> page;
+	private ProductDTO productDTO;
+	
+	@BeforeEach
+	void setup() throws Exception {
+		
+		productDTO = Factory.createProductDTO();
+		page = new PageImpl<>(List.of(productDTO));
+		
+		when(service.findAllPaged(ArgumentMatchers.any())).thenReturn(page);
+	}
+	
+	@Test
+	public void findAllShouldReturnPage() throws Exception{
+		mockMvc.perform(get("/products")).andExpect(status().isOk());
+	}
+
+}
