3d083e9ebb385bf2cdcd44236085d2b825acdbcc
Artur Tarcisio <arturtarcisio1@gmail.com>
2022-05-17 08:18:50 -0300

    Teste de integracao na camada web update

--- backend/src/test/java/io/github/dscatalog/resources/ProductResourceIntegrationTests.java
+++ backend/src/test/java/io/github/dscatalog/resources/ProductResourceIntegrationTests.java
@@ -1,6 +1,7 @@
 package io.github.dscatalog.resources;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,7 +12,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.dscatalog.dto.ProductDTO;
+import io.github.dscatalog.tests.Factory;
 
 @SpringBootTest
 @Transactional
@@ -19,21 +26,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductResourceIntegrationTests {
 	
 	private final MockMvc mockMvc;
+	private final ObjectMapper objectMapper;
 
 	@Autowired
-	public ProductResourceIntegrationTests(MockMvc mockMvc) {
+	public ProductResourceIntegrationTests(MockMvc mockMvc, ObjectMapper objectMapper) {
 		this.mockMvc = mockMvc;
+		this.objectMapper = objectMapper;
 	}
 	
-	private long existingId;
-	private long nonExistentId;
+	private Long existingId;
+	private Long nonExistentId;
 	private Long countTotalProduct;
+	private ProductDTO productDTO;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistentId = 900L;
 		countTotalProduct = 25L;
+		productDTO = Factory.createProductDTO();
 	}
 	
 	@Test
@@ -48,6 +59,43 @@ public class ProductResourceIntegrationTests {
 		resultActions.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
 		resultActions.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
 		resultActions.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+	}
+	
+	@Test
+	public void updateShouldThrowNotFoundWhenIdDoesNotExist() throws Exception{
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+		ResultActions result = mockMvc.perform(put("/products/{id}", nonExistentId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void updateShouldUpdateWhenIdExist() throws Exception{
+		
+		String expectedName = productDTO.getName();
+		String expectedDescription = productDTO.getDescription();
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		ResultActions result = mockMvc
+				.perform(put("/products/{id}", existingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.id").value(existingId));
+		
+		result.andExpect(jsonPath("$.name").exists());
+		result.andExpect(jsonPath("$.name").value(expectedName));
+		
+		result.andExpect(jsonPath("$.description").exists());
+		result.andExpect(jsonPath("$.description").value(expectedDescription));
 	}
 	
 	
